2d274c282219d5844a8b29dcb927b804ef8f0723
fetthesten <coercion@online.no>
2018-05-24 15:24:47 +0200

    yes

--- scr/Player.gd
+++ scr/Player.gd
@@ -1,20 +1,18 @@
-extends Node2D
-
-onready var mover = $KinematicBody2D
+extends 'res://scr/mover.gd'
 
 func _ready():
 
 	pass
 
 func _process(delta):
-	var move = Main.V2_ZERO
+	var movement = Main.V2_ZERO
 	if Input.is_action_pressed('ui_up'):
-		move.y = -1.0
+		movement.y = -1.0
 	elif Input.is_action_pressed('ui_down'):
-		move.y = 1.0
+		movement.y = 1.0
 	if Input.is_action_pressed('ui_left'):
-		move.x = -1.0
+		movement.x = -1.0
 	elif Input.is_action_pressed('ui_right'):
-		move.x = 1.0
-	if move != Main.V2_ZERO:
-		mover.move(move)
+		movement.x = 1.0
+	if movement != Main.V2_ZERO:
+		move(movement)
